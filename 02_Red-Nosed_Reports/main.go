9fcff2c3c426b449883206e357259f6d8d6be5d3
sguerrini97 <sguerrini97@protonmail.com>
2024-12-02 14:27:51 +0100

    solved day2 / part 2

--- 02_Red-Nosed_Reports/main.go
+++ 02_Red-Nosed_Reports/main.go
@@ -74,14 +74,86 @@ func part1(input_file *os.File) int {
 }
 
 func part2(input_file *os.File) int {
+	safe := 0
+
 	// read file line by line
 	scanner := bufio.NewScanner(input_file)
 	for scanner.Scan() {
-		line := scanner.Text()
-		fmt.Println(line)
+		readings := strings.Split(scanner.Text(), " ")
+
+		good := true
+		skip := -1
+
+		// skip each step as long as we get an error
+		for skip < len(readings) {
+			previous_val := 0
+			good = true
+			increasing := false
+			decreasing := false
+
+			for i := range readings {
+				if i == skip {
+					// skip this step
+					continue
+				}
+
+				val, _ := strconv.Atoi(readings[i])
+
+				// don't check anything on the first value
+				if i == 0 || (i == 1 && skip == 0) {
+					previous_val = val
+					continue
+				}
+
+				// not safe because value changed too much
+				distance := val - previous_val
+				if distance > 3 || distance < -3 {
+					good = false
+					break
+				}
+
+				// check if we are still increasing/decreasing
+				if distance > 0 {
+					increasing = true
+					if decreasing {
+						// fail because we changed direction
+						good = false
+						break
+					}
+				} else if distance < 0 {
+					decreasing = true
+					if increasing {
+						// fail because we changed direction
+						good = false
+						break
+					}
+				} else {
+					// fail because consecutive values must change
+					good = false
+					break
+				}
+
+				// store current value
+				previous_val = val
+			}
+
+			// if we didn't break there is no problem with this test
+			// and there's no need to continue
+			if good {
+				break
+			}
+
+			// skip the next value and check the test again
+			skip += 1
+		}
+
+		// if we didn't break before this test is safe
+		if good {
+			safe += 1
+		}
 	}
 
-	return 0
+	return safe
 }
 
 func main() {
