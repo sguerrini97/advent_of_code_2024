37955fba494c1007521f9e4c4005d768674a9849
sguerrini97 <sguerrini97@protonmail.com>
2024-12-05 21:25:30 +0100

    solved day5 / part 1

--- 05_Print_Queue/main.go
+++ 05_Print_Queue/main.go
@@ -6,16 +6,19 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
-func parse(input_path *string) {
-
+func parse(input_path *string) (map[int][]int, [][]int) {
 	input_file, err := os.Open(*input_path)
 	if err != nil {
 		log.Fatalf("Failed to open input file \"%s\"\nError: %s\n", *input_path, err)
 	}
 
 	defer input_file.Close()
+
+	constraints := make(map[int][]int)
 
 	// read page order section of input
 	scanner := bufio.NewScanner(input_file)
@@ -31,19 +34,69 @@ func parse(input_path *string) {
 		var y int
 
 		fmt.Sscanf(line, "%d|%d", &x, &y)
+
+		// create a map of all the values the pages that cannot be after a given page
+		_, present := constraints[y]
+		if !present {
+			constraints[y] = make([]int, 0)
+		}
+		constraints[y] = append(constraints[y], x)
 	}
+
+	updates := make([][]int, 0)
 
 	// read updates section of the input
 	for scanner.Scan() {
+		update_strs := strings.Split(scanner.Text(), ",")
+
+		update := make([]int, len(update_strs))
+
+		for ui, update_str := range update_strs {
+			value, _ := strconv.Atoi(update_str)
+			update[ui] = value
+		}
+
+		updates = append(updates, update)
 	}
+
+	return constraints, updates
 }
 
-func part1() int {
+func part1(constraints map[int][]int, updates [][]int) int {
+	sum := 0
 
-	return 0
+	// check each update
+	for _, update := range updates {
+		good := true
+		forbidden := make(map[int]bool)
+
+		for _, page := range update {
+			// if this page cannot appear, this update is not valid
+			_, present := forbidden[page]
+			if present {
+				good = false
+				break
+			}
+
+			// check to add restriction for pages after this one
+			to_forbid, present := constraints[page]
+			if present {
+				for _, forbidden_page := range to_forbid {
+					forbidden[forbidden_page] = true
+				}
+			}
+		}
+
+		if good {
+			i := len(update) / 2
+			sum += update[i]
+		}
+	}
+
+	return sum
 }
 
-func part2() int {
+func part2(constraints map[int][]int, updates [][]int) int {
 
 	return 0
 }
@@ -58,13 +111,14 @@ func main() {
 		log.Fatalf("Invalid challenge part %d", *part)
 	}
 
-	parse(input_path)
+	// parse input files into structured data
+	constraints, updates := parse(input_path)
 
 	result := -1
 	if *part == 1 {
-		result = part1()
+		result = part1(constraints, updates)
 	} else if *part == 2 {
-		result = part2()
+		result = part2(constraints, updates)
 	}
 
 	fmt.Printf("Solution: %d\n", result)
